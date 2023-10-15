6a5826c28ca7dc6b414257d856c42754147e607e
MsDiala <diala-is-diala@hotmail.com>
2023-10-15 21:15:12 +0100

    done

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
 // Write your code here
+
+// Challenge 1: Multiply
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+console.log(multiply); // Output should be 62
+
+// Challenge 2: Random
+const random = Math.floor(Math.random() * 100) + 1; // Generates a random integer between 1 and 100
+console.log(random);
+
+// Challenge 3: Mod
+const num3 = 40;
+const num4 = 36;
+const mod = num3 % num4;
+console.log(mod); // Output should be 4
+
+// Challenge 4: Max
+const numbers = [2, 5, 12, 8, 20, 3];
+const max = Math.max(...numbers); // Finds the highest number in the array
+console.log(max); // Output should be 20
